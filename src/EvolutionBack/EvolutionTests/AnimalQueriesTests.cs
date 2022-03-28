6be9de7d798dea9febf01e84512b7b26be6d487e
Sergey Gorshkov <Sergey.Gorshkov@monitel.com>
2022-03-28 13:35:11 +0300

    Tests

---
+++ src/EvolutionBack/EvolutionTests/AnimalQueriesTests.cs
@@ -0,0 +1,24 @@
+﻿using EvolutionBack.Services;
+using System.Linq;
+using Xunit;
+
+namespace EvolutionTests;
+
+public class AnimalQueriesTests
+{
+    [Fact]
+    public void Get_animals()
+    {
+        // создаем сервис
+        var animalQueries = new AnimalQueries();
+        // вызываем метод сервиса
+        var animals = animalQueries.GetAnimals().ToList();
+        // проверяем полученный список
+        Assert.Equal(5, animals.Count);
+        for (int i = 0; i < animals.Count; i++)
+        {
+            // проверяем свойства каждого элемента
+            Assert.Equal($"animal {i + 1}", animals[i].Name);
+        }
+    }
+}
