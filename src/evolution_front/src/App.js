e3edd6dff7393854c1f4ef03f3e967f0b4750be6
Sergey Gorshkov <sergik-gorshkov@mail.ru>
2022-04-12 23:28:40 +0300

    login page

--- src/evolution_front/src/App.js
+++ src/evolution_front/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Menu from 'components/Menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/menu">
+            <Menu />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
