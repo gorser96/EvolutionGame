8c99e0366fab435a6f6031fae27ab946f1d4899d
Sergey Gorshkov <Sergey.Gorshkov@monitel.com>
2022-04-12 14:05:52 +0300

    db update for Start, end, next step in room

---
+++ src/EvolutionBack/EvolutionBack/Migrations/20220412110519_update_0003.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EvolutionBack.Migrations
+{
+    public partial class update_0003 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PauseStartedTime",
+                table: "Rooms",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDateTime",
+                table: "Rooms",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "InGameUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PauseStartedTime",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "StartDateTime",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "InGameUsers");
+        }
+    }
+}
