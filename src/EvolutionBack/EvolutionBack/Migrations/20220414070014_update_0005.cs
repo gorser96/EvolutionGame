67a1d00b48176260223f25c2bfb2ff181a18ce74
Sergey Gorshkov <Sergey.Gorshkov@monitel.com>
2022-04-14 10:01:06 +0300

    Db migration. IsHost in the table InGameUsers

---
+++ src/EvolutionBack/EvolutionBack/Migrations/20220414070014_update_0005.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EvolutionBack.Migrations
+{
+    public partial class update_0005 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHost",
+                table: "InGameUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsHost",
+                table: "InGameUsers");
+        }
+    }
+}
