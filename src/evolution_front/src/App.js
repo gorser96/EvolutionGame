3c5295652cd4209a603b783bdbcd38eadca45a49
Sergey Gorshkov <Sergey.Gorshkov@monitel.com>
2022-04-21 16:56:40 +0300

    Auth context

--- src/evolution_front/src/App.js
+++ src/evolution_front/src/App.js
@@ -1,21 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Menu from 'components/Menu';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Menu from './components/Menu';
+
+const AuthContext = React.createContext();
+
+const fakeAuth = () =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve('2342f2f1d131rf12'), 250);
+  });
+
+const AuthProvider = ({ children }) => {
+  const [token, setToken] = React.useState(null);
+
+  const handleLogin = async () => {
+    const token = await fakeAuth();
+
+    setToken(token);
+  };
+
+  const handleLogout = () => {
+    setToken(null);
+  };
+
+  const value = {
+    token,
+    onLogin: handleLogin,
+    onLogout: handleLogout,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => {
+  return React.useContext(AuthContext);
+};
 
 function App() {
   return (
-    <div className="wrapper">
-      <h1>Application</h1>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/menu">
-            <Menu />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <AuthProvider>
+
+      <div>
+        <h1>Application</h1>
+
+        <Navigation />
+
+        <Routes>
+          <Route path="menu" element={<Menu />} />
+        </Routes>
+      </div>
+
+    </AuthProvider>
   );
 }
+
+const Navigation = () => {
+  const { token, onLogout } = useAuth();
+  return (
+    <nav>
+      <NavLink to="/menu">Menu</NavLink>
+      {token && (
+        <button type="button" onClick={onLogout}>
+          Sign Out
+        </button>
+      )}
+    </nav>
+  );
+};
 
 export default App;
--- src/evolution_front/src/components/Menu.js
+++ src/evolution_front/src/components/Menu.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import useAuth from '../App'
 
 export default function Menu() {
+  const { onLogin } = useAuth();
+  
   return(
-    <h2>Menu</h2>
+    <>
+      <h2>Menu</h2>
+
+      <button type="button" onClick={onLogin}>Sign In</button>
+    </>
   );
 }
