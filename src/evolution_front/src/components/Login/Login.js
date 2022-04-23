50ed8a47736cecf15f3d78c6e5ad3aabf3ccf610
Sergey Gorshkov <sergik-gorshkov@mail.ru>
2022-04-23 15:57:10 +0300

    Login page with logo

--- src/evolution_front/src/components/Login/Login.js
+++ src/evolution_front/src/components/Login/Login.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import './Login.css'
+import logo from '../../img/gekkon.png'
 
 export class Login extends Component {  
   render () {
     return (
       <div className='fill-window text-center'>
         <form>
-          <h1 className='h3 mb-3 font-weight-normal'>Please sign in</h1>
-          <label className='sr-only'>Login</label>
-          <input type="email" className="form-control"  aria-describedby="loginHelp" placeholder="Enter login" />
-          <label className='sr-only'>Password</label>
-          <input type="password" className="form-control" placeholder="Password" />
-          <button type="submit" className="btn btn-lg btn-primary btn-block mt-3">Login</button>
+          <div className='evo-name-block'>
+            <img className='mb-4 evo-logo me-3 ms-4' src={logo} width="72" height="72" />
+            <div className='evo-name'>EVOLUTION</div>
+          </div>
+          <input type="text" className="form-control"  aria-describedby="loginHelp" placeholder="Enter login" />
+          <input type="password" className="mt-1 form-control" placeholder="Password" />
+          <button type="submit" className="btn btn-lg btn-success btn-block mt-3">Вход</button>
         </form>
       </div>
     );
