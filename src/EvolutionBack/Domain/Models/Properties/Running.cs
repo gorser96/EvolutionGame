b6d483ade35353255a5d5c1076ea08bbec14025d
Tip4ik26 <Tip4ik26@gmail.com>
2022-05-03 18:56:56 +0300

    Propirties (2 свойства)

---
+++ src/EvolutionBack/Domain/Models/Properties/Running.cs
@@ -0,0 +1,32 @@
+﻿namespace Domain.Models;
+
+public class Running : Property, IPropertyAction
+{
+    public Running(Guid uid, string name, bool isPair, bool isOnEnemy)
+        : base(uid, name, isPair, isOnEnemy, 0, nameof(Running))
+    {
+        IsActive = true;
+    }
+
+    public bool IsActive { get; private set; }
+
+    public void SetIsActive(bool value)
+    {
+        IsActive = value;
+    }
+
+    public bool? OnDefense(Animal self, Animal enemy)
+    {
+        Random rand=new Random();
+       var cubeResult = rand.Next(1,6);
+        if (cubeResult >= 4)
+        {
+            //Если выпали числа 4,5,6
+        }
+        return false;
+    }
+
+    public void OnUse(Animal self, Animal? target = null)
+    {
+    }
+}
---
+++ src/EvolutionBack/Domain/Models/Properties/Swimming.cs
@@ -0,0 +1,31 @@
+﻿namespace Domain.Models;
+
+public class Swimming : Property, IPropertyAction
+{
+    public Swimming(Guid uid, string name, bool isPair, bool isOnEnemy)
+        : base(uid, name, isPair, isOnEnemy, 0, nameof(Swimming))
+    {
+        IsActive = true;
+    }
+
+    public bool IsActive { get; private set; }
+
+    public void SetIsActive(bool value)
+    {
+        IsActive = value;
+    }
+
+    public bool? OnDefense(Animal self, Animal enemy)
+    {
+        if (enemy.Properties.Any(x=>x.Property.AssemblyName == nameof(Swimming)))
+        {
+         //Если атакующий водоплавающее
+        }
+
+        return null;
+    }
+
+    public void OnUse(Animal self, Animal? target = null)
+    {
+    }
+}
