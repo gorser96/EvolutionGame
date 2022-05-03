d45c65744a800009cf2123728e6f5573bedef183
Tip4ik26 <Tip4ik26@gmail.com>
2022-05-03 18:56:56 +0300

    Propirties (2 свойства)

---
+++ src/EvolutionBack/Domain/Models/Properties/Running.cs
@@ -0,0 +1,33 @@
+﻿namespace Domain.Models;
+
+public class Running : Property, IPropertyAction
+{
+    public Running(Guid uid, string name, bool isPair, bool isOnEnemy)
+        : base(uid, name, isPair, isOnEnemy, 0, nameof(Running))
+    {
+        IsActive = true;
+    }
+
+    public bool IsActive { get; private set; }
+
+    public void SetIsActive(bool value)
+    {
+        IsActive = value;
+    }
+
+    public bool? OnDefense(Animal self, Animal enemy)
+    {
+        Random rand=new Random();
+       var cubeResult = rand.Next(1,6);
+        if (cubeResult >= 4)
+        {
+            //Если выпали числа 4,5,6
+            return true;
+        }
+        return false;
+    }
+
+    public void OnUse(Animal self, Animal? target = null)
+    {
+    }
+}
