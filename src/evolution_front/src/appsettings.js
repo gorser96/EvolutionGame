eeb3a4db32aa9af2c992d758626486c76dc8d0ef
Sergey <sergik-gorshkov@mail.ru>
2024-02-10 13:40:24 +0300

    Замена проекта

--- src/evolution_front/src/appsettings.js
+++
@@ -1,4 +0,0 @@
-const apiUrl = 'http://localhost:5147/api';
-const hubUrl = 'http://localhost:5147/api/hub'
-
-export { apiUrl, hubUrl };
--- src/evolution_front/src/constants/system.constants.js
+++
@@ -1,9 +0,0 @@
-export const systemConstants = {
-  SNACK_NOTIFY: "SNACK_NOTIFY",
-};
-
-export const NotifySeverity = {
-  Success: "success",
-  Error: "error",
-  Warning: "warning",
-};
